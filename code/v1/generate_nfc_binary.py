6f6440564a936eef0540282a736e62f7ed2c14d3
Andy Gong <45613281+AG2048@users.noreply.github.com>
2025-07-29 01:40:30 -0400

    Add script outline for NFC binary generation
    Created initial Python script with comments describing the intended functionality for generating a binary file for NFC tags from image and timing data.

---
+++ code/v1/generate_nfc_binary.py
@@ -0,0 +1,5 @@
+# This script generates the binary file that can be written to the NFC tag to be displayed.
+
+# Input: list of images / gifs, and associated colour / timing data.
+
+# Output: hex binary file for NFC tag
